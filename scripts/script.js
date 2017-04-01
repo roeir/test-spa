bd4a9ad077a9c85bfc1a9e717252c44b815c1cbb
Andrii Skrebets <skrebets.andrew@yandex.ru>
2017-04-01 19:08:13 +0300

    add insert method

--- scripts/script.js
+++ scripts/script.js
@@ -27,6 +27,137 @@ class Router {
     }
 }
 
+const store = {
+    get(key) {
+        return localStorage.getItem(key)
+    },
+    set(key, value) {
+        return localStorage.setItem(key, value);
+    }
+};
+
+class Model {
+    defineModel(options) {
+        this[options.name] = new Collection(options, this);
+    }
+}
+
+class Collection {
+    constructor(options, rootModel) {
+        this.__rootModel = rootModel;
+        this.__name = options.name;
+        this.__fields = options.fields;
+        this.__data = {};
+            // this.__getInitialData();
+    }
+
+    insert(data) {
+        if(this.__validateData(data)) {
+            this.__data[data.id] = data;
+            this.__saveData(data);
+        } else {
+            throw new Error({
+                message: 'Bad data',
+                data
+            });
+        }
+    }
+
+    __getInitialData() {
+        try{
+            const data = store.get(this.__name);
+            return JSON.parse(data);
+        } catch (err) {
+            console.log('Failed to load data');
+        }
+    }
+
+    __saveData(data) {
+        try {
+            store.set(this.__name, JSON.stringify(this.__data));
+        } catch (err) {
+            console.log('Saving error', this.__data);
+        }
+    }
+
+    __validateData(data) {
+        const dataProps = Object.keys(data);
+        return dataProps.every(prop => {
+            const field = this.__fields[prop];
+            if (!field) {
+                return false;
+            }
+            if(field.type && !(field.type === typeof data[prop])) {
+                return false;
+            }
+            if(field.presence && !data[prop]) {
+                return false;
+            }
+
+            return true;
+        });
+    }
+}
+
+const model = new Model();
+
+model.defineModel({
+    name: 'author',
+    fields: {
+        id: {type: 'string'},
+        fullName: {type: 'string', defaultTo: '', presence: true},
+        avatarUrl: {type: 'string', defaultTo: 'http://placehold.it/100x300'},
+        dateOfDeath: {type: 'string', defaultTo: ''},
+        city: {type: 'string', defaultTo: ''},
+        books: {ref: 'book'}
+    }
+});
+model.defineModel({
+    name: 'book',
+    fields: {
+        id: {type: 'string'},
+        title: {type: 'string', defaultTo: ''},
+        image: {type: 'string', defaultTo: 'http://placehold.it/100x300'},
+        genre: {type: 'string', defaultTo: ''},
+        year: {type: 'string', defaultTo: ''},
+        authors: {ref: 'author'}
+    }
+});
+
+model.author.insert({
+    id: '1',
+    fullName: 'Death Man Bill',
+    avatarUrl: '',
+    dateOfDeath: '',
+    city: '',
+    books: ['1']
+});
+model.author.insert({
+    id: '2',
+    fullName: 'Dead Star',
+    avatarUrl: '',
+    dateOfDeath: '',
+    city: '',
+    books: ['2']
+});
+
+model.book.insert({
+    id: '1',
+    title: 'Book of Death Man',
+    image: '',
+    genre: 'Novel',
+    year: '2000',
+    authors: ['1']
+});
+model.book.insert({
+    id: '2',
+    title: 'Book of Dead Star',
+    image: '',
+    genre: 'Novel Drama',
+    year: '2001',
+    authors: ['2']
+});
+
 function p(elem, props, children) {
     const addPropsToElem = (elem, props) => {
         if(props) {
@@ -92,13 +223,15 @@ function renderHeader() {
             p('div', {className: 'site-logo'}, [
                 p('a', {href: '#', textContent: 'Dp'})
             ]),
-            p('div', {className: 'links'}, [
+            p('div', {className: 'site-links'}, [
                 p('a', {href: '#/authors', textContent: 'Authors'}),
                 p('a', {href: '#/books', textContent: 'Books'}),
             ])
         ])
     ]);
 }
+
+
 
 const router = new Router();
 router.add('/', renderRoot);
